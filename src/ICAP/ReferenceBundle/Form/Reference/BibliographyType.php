b15c6865bbf411037f5115708d55f9a8108048d0
eddymarques <eddy.marques@gmail.com>
2013-02-27 18:00:22 +0100

    pop info on referebce list

--- src/ICAP/ReferenceBundle/Form/Reference/BibliographyType.php
+++ src/ICAP/ReferenceBundle/Form/Reference/BibliographyType.php
@@ -10,14 +10,14 @@ class BibliographyType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('year', 'integer')
-            ->add('language')
-            ->add('author')
-            ->add('summary', 'textarea')
-            ->add('isbn')
-            ->add('editor')
-            ->add('publisher')
-            ->add('pages')
+            ->add('year', 'integer', array('required' => false ))
+            ->add('language', null, array('required' => false ))
+            ->add('author', null, array('required' => false ))
+            ->add('summary', 'textarea', array('required' => false ))
+            ->add('isbn', null, array('required' => false ))
+            ->add('editor', null, array('required' => false ))
+            ->add('publisher', null, array('required' => false ))
+            ->add('pages', null, array('required' => false ))
         ;
     }
 
